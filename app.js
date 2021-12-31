14fdfb4ff704fc8f5b6d5755137e5610ef996036
mitrovicbojan <mitrovicbojan87@gmail.com>
2021-12-31 15:07:04 +0200

    btn

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-let firstCard = 6;
-let secondCard = 9;
+let firstCard = 11;
+let secondCard = 5;
+
 let hasBlackjack = false;
 let isAlive = true;
 let message = ""
-let sum = firstCard + secondCard;
+let sum = firstCard + secondCard
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 
 function startGame() {
+
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if(sum <= 20) {
-        message = "do you want another card?"    
+        message = "Do you want another card?"    
     } else if(sum === 21) {
-        message = "you have got Blackjack!";
+        message = "You have got Blackjack!";
         hasBlackjack = true;
     } else {
         message = "You have lost!"
         isAlive = false;
     }
+    messageEl.textContent = message
+}
+function newCard() {
+    let newCards = 3
+    sum += newCards
+    startGame()
 }
